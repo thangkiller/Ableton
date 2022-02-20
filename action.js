0a67c4b2863ff76f3e44cba5f2c376df27a5d29b
Nguyen Thang <ngenvan000@gmail.com>
2022-02-20 22:06:22 +0700

    update

---
+++ action.js
@@ -0,0 +1,10 @@
+var nav2Node = document.querySelector('.navbarSecondary');
+var navNode = document.querySelector('.navbar');
+var yOld = navNode.getBoundingClientRect().y;
+
+document.onscroll = function() {
+	var yCur = navNode.getBoundingClientRect().y;
+	if(yCur > yOld) nav2Node.classList.add('navbarSecondary--disappear');
+	else nav2Node.classList.remove('navbarSecondary--disappear');
+	yOld = yCur;
+};
