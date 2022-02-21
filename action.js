4f5e75cd7473bca6235c1001cfdca3f212006462
Nguyen Thang <ngenvan000@gmail.com>
2022-02-21 19:26:41 +0700

    update

--- action.js
+++ action.js
@@ -11,7 +11,10 @@ document.onscroll = function() {
 
 
 //button menu
-var menuNode = document.querySelector('.screen__menu');
-document.onclick = function(){
-	menuNode.classList.toggle('')
+var btnNode = document.querySelector('.screen--short__navbar__button');
+var menuNode = document.querySelector('.screen--short__menu');
+var navNode = document.querySelector('.screen--short');
+btnNode.onclick = function(){
+	menuNode.classList.toggle('menu--disappear');
+	navNode.classList.toggle('menu--changed');
 }
